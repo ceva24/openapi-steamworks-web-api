2620f874f7c5ac28a72f495374248bb8ad636629
ceva24 <chris@ceva24.dev>
2021-05-23 15:39:32 +0100

    Add a snapshot test for the OpenAPI spec

--- src/openapi/openapi-spec-creator.test.js
+++ src/openapi/openapi-spec-creator.test.js
@@ -2,6 +2,12 @@ import { SPEC_VERSION, SPEC_INFO_TITLE } from "../constants/constants.js";
 import { createOpenApiSpec } from "./openapi-spec-creator.js";
 
 describe("openapi spec creator", () => {
+    it("creates the openapi spec", () => {
+        const openApiSpec = createOpenApiSpec();
+
+        expect(openApiSpec).toMatchSnapshot();
+    });
+
     it("sets the version number", () => {
         const openApiSpec = createOpenApiSpec();
 
