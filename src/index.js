a0faf0b83bcb1a03a2f557886a6bb852356284e2
ceva24 <chris@ceva24.dev>
2021-05-23 19:22:14 +0100

    Update main function to create and write the OpenAPI spec

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+import "./setup-dotenv.js";
 import { fetchApiDefinition } from "./steam/api-definition-fetcher.js";
+import { createOpenApiSpec } from "./openapi/openapi-spec-creator.js";
+import { writeOpenApiSpec } from "./file/spec-writer.js";
 
 const run = async () => {
-    const json = await fetchApiDefinition();
+    console.log("Starting");
 
-    console.log(json);
+    console.log("Fetching Steamworks Web API definition...");
+    await fetchApiDefinition();
+
+    console.log("Creating OpenAPI spec...");
+    const openApiSpec = createOpenApiSpec();
+
+    console.log("Writing to output file...");
+    writeOpenApiSpec(openApiSpec);
+
+    console.log("Complete");
 };
 
 run();
