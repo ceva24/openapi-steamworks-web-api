bf9d892c53634cdff01e93a9da92c93e1cf31059
ceva24 <chris@ceva24.dev>
2021-05-23 16:43:44 +0100

    Add empty paths field to OpenAPI Spec

--- src/openapi/openapi-spec-creator.js
+++ src/openapi/openapi-spec-creator.js
@@ -2,7 +2,7 @@ import { SPEC_VERSION } from "../constants/constants.js";
 import { createInfo } from "./components/info-creator.js";
 
 const createOpenApiSpec = () => {
-    return { openapi: SPEC_VERSION, info: createInfo() };
+    return { openapi: SPEC_VERSION, info: createInfo(), paths: {} };
 };
 
 export { createOpenApiSpec };
