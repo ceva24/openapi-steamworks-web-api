74fc9dddcba16450ffefc136c46f89ebf29fa478
ceva24 <chris@ceva24.dev>
2021-05-25 21:05:24 +0100

    Validate the OpenAPI spec

--- src/steam/api-definition-fetcher.js
+++ src/steam/api-definition-fetcher.js
@@ -3,9 +3,8 @@ import { STEAM_API_URL } from "../constants/constants.js";
 
 const fetchApiDefinition = async () => {
     const response = await fetch(STEAM_API_URL);
-    const json = await response.json();
 
-    return json;
+    return response.json();
 };
 
 export { fetchApiDefinition };
