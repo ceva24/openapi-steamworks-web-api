aef93e171b728473cb1e2d7141e8fe2e7cf8d9c5
ceva24 <chris@ceva24.dev>
2021-05-28 18:13:39 +0100

    Refactor path parameter creation

--- src/openapi/components/paths/path-creator.js
+++ src/openapi/components/paths/path-creator.js
@@ -1,11 +1,8 @@
 import {
     DEFAULT_RESPONSES,
-    SPEC_FORMATS,
-    SPEC_PATHS_PARAMETERS_IN,
-    SPEC_TYPES,
-    STEAM_PARAMETER_TYPES,
     STEAM_EXTERNAL_DOCS_URL,
 } from "../../../constants/constants.js";
+import { createPathParameters } from "./path-parameters-creator.js";
 
 const createPath = (interfaceName, method) => {
     const key = `/${interfaceName}/${method.name}/v${method.version}`;
@@ -18,7 +15,7 @@ const createPath = (interfaceName, method) => {
             parameters:
                 httpMethodName === "get" &&
                 !interfaceName.toLowerCase().includes("service")
-                    ? createParameters(method.parameters)
+                    ? createPathParameters(method.parameters)
                     : [],
             externalDocs: {
                 url: `${STEAM_EXTERNAL_DOCS_URL}/${interfaceName}#${method.name}`,
@@ -28,33 +25,6 @@ const createPath = (interfaceName, method) => {
     };
 
     return { [key]: value };
-};
-
-const createParameters = (parameters) => {
-    return parameters.map((parameter) => {
-        return {
-            name: parameter.name,
-            in: SPEC_PATHS_PARAMETERS_IN,
-            description: parameter.description,
-            required: !parameter.optional,
-            schema: createSchema(parameter.type),
-        };
-    });
-};
-
-const createSchema = (steamType) => {
-    switch (steamType) {
-        case STEAM_PARAMETER_TYPES.UINT64:
-            return { type: SPEC_TYPES.INTEGER, format: SPEC_FORMATS.INT64 };
-        case STEAM_PARAMETER_TYPES.UINT32:
-            return { type: SPEC_TYPES.INTEGER, format: SPEC_FORMATS.INT32 };
-        case STEAM_PARAMETER_TYPES.INT32:
-            return { type: SPEC_TYPES.INTEGER, format: SPEC_FORMATS.INT32 };
-        case STEAM_PARAMETER_TYPES.RAWBINARY:
-            return { type: SPEC_TYPES.STRING, format: SPEC_FORMATS.BINARY };
-        default:
-            return { type: SPEC_TYPES.STRING };
-    }
 };
 
 export { createPath };
