634d99f8a592beadccaa8fc82ce6e750609d7981
ceva24 <chris@ceva24.dev>
2021-05-28 23:17:01 +0100

    Create request body required property

--- src/openapi/components/paths/path-request-body-creator.js
+++ src/openapi/components/paths/path-request-body-creator.js
@@ -1,15 +1,34 @@
 import { SPEC_PATHS_REQUEST_BODY_CONTENT_TYPE } from "../../../constants/constants.js";
 
-const createRequestBody = (httpMethod) => {
-    if (httpMethod !== "post") return null;
+const createRequestBody = (httpMethod, parameters) => {
+    if (httpMethod !== "post" || parameters.length === 0) return null;
 
     return {
         content: {
             [SPEC_PATHS_REQUEST_BODY_CONTENT_TYPE]: {
-                schema: { type: "object" },
+                schema: createSchema(parameters),
             },
         },
     };
+};
+
+const createSchema = (parameters) => {
+    const schema = { type: "object" };
+
+    const required = createRequired(parameters);
+    if (required.length > 0) schema.required = required;
+
+    return schema;
+};
+
+const createRequired = (parameters) => {
+    return parameters
+        .filter((parameter) => {
+            return !parameter.optional;
+        })
+        .map((parameter) => {
+            return parameter.name;
+        });
 };
 
 export { createRequestBody };
--- src/openapi/components/paths/path-request-body-creator.test.js
+++ src/openapi/components/paths/path-request-body-creator.test.js
@@ -18,6 +18,12 @@ describe("path request body creator", () => {
         expect(requestBody).toBeNull();
     });
 
+    it("does not create a request body for requests with no parameters", () => {
+        const requestBody = createRequestBody("post", []);
+
+        expect(requestBody).toBeNull();
+    });
+
     it("sets the content key", () => {
         const httpMethod = "post";
         const parameters = [
@@ -53,5 +59,91 @@ describe("path request body creator", () => {
             requestBody.content[Object.keys(requestBody.content)[0]];
 
         expect(mediaType.schema.type).toEqual("object");
+    });
+
+    it("sets a required property", () => {
+        const httpMethod = "post";
+        const parameters = [
+            {
+                name: "collectioncount",
+                type: "uint32",
+                optional: false,
+                description: "Number of collections being requested",
+            },
+        ];
+
+        const requestBody = createRequestBody(httpMethod, parameters);
+        const mediaType =
+            requestBody.content[Object.keys(requestBody.content)[0]];
+
+        expect(mediaType.schema.required).toHaveLength(1);
+        expect(mediaType.schema.required[0]).toEqual("collectioncount");
+    });
+
+    it("sets multiple required properties", () => {
+        const httpMethod = "post";
+        const parameters = [
+            {
+                name: "collectioncount",
+                type: "uint32",
+                optional: false,
+                description: "Number of collections being requested",
+            },
+            {
+                name: "publishedfileids[0]",
+                type: "uint64",
+                optional: false,
+                description: "collection ids to get the details for",
+            },
+        ];
+
+        const requestBody = createRequestBody(httpMethod, parameters);
+        const mediaType =
+            requestBody.content[Object.keys(requestBody.content)[0]];
+
+        expect(mediaType.schema.required).toHaveLength(2);
+    });
+
+    it("does not set required when there are no required properties", () => {
+        const httpMethod = "post";
+        const parameters = [
+            {
+                name: "pollid",
+                type: "uint32",
+                optional: true,
+                description: "Caller-specific poll id",
+            },
+        ];
+
+        const requestBody = createRequestBody(httpMethod, parameters);
+        const mediaType =
+            requestBody.content[Object.keys(requestBody.content)[0]];
+
+        expect(mediaType.schema).not.toHaveProperty("required");
+    });
+
+    it("does not set optional properties as required", () => {
+        const httpMethod = "post";
+        const parameters = [
+            {
+                name: "message",
+                type: "uint32",
+                optional: false,
+                description: "Message that was last known to the user",
+            },
+            {
+                name: "pollid",
+                type: "uint32",
+                optional: true,
+                description: "Caller-specific poll id",
+            },
+        ];
+
+        const requestBody = createRequestBody(httpMethod, parameters);
+        const mediaType =
+            requestBody.content[Object.keys(requestBody.content)[0]];
+
+        expect(mediaType.schema.required).toHaveLength(1);
+        expect(mediaType.schema.required[0]).toEqual("message");
     });
 });
