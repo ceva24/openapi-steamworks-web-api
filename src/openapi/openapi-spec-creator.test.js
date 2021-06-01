b05a131a7ac37228c5ea6deaa4fa4e1c5acd9cac
ceva24 <chris@ceva24.dev>
2021-06-01 02:16:00 +0100

    Update snapshot test with some service interfaces

--- src/openapi/openapi-spec-creator.test.js
+++ src/openapi/openapi-spec-creator.test.js
@@ -64,6 +64,98 @@ describe("openapi spec creator", () => {
                             },
                         ],
                     },
+                    {
+                        name: "IContentServerDirectoryService",
+                        methods: [
+                            {
+                                name: "GetServersForSteamPipe",
+                                version: 1,
+                                httpmethod: "GET",
+                                parameters: [
+                                    {
+                                        name: "cell_id",
+                                        type: "uint32",
+                                        optional: false,
+                                        description: "client Cell ID",
+                                    },
+                                    {
+                                        name: "max_servers",
+                                        type: "uint32",
+                                        optional: true,
+                                        description:
+                                            "max servers in response list",
+                                    },
+                                    {
+                                        name: "ip_override",
+                                        type: "string",
+                                        optional: true,
+                                        description: "client IP address",
+                                    },
+                                    {
+                                        name: "launcher_type",
+                                        type: "int32",
+                                        optional: true,
+                                        description: "launcher type",
+                                    },
+                                    {
+                                        name: "ipv6_public",
+                                        type: "string",
+                                        optional: true,
+                                        description:
+                                            "client public ipv6 address if it knows it",
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                    {
+                        name: "IGameNotificationsService",
+                        methods: [
+                            {
+                                name: "UserCreateSession",
+                                version: 1,
+                                httpmethod: "POST",
+                                description: "Creates an async game session",
+                                parameters: [
+                                    {
+                                        name: "appid",
+                                        type: "uint32",
+                                        optional: false,
+                                        description:
+                                            "The appid to create the session for.",
+                                    },
+                                    {
+                                        name: "context",
+                                        type: "uint64",
+                                        optional: false,
+                                        description:
+                                            "Game-specified context value the game can used to associate the session with some object on their backend.",
+                                    },
+                                    {
+                                        name: "title",
+                                        type: "{message}",
+                                        optional: false,
+                                        description:
+                                            "The title of the session to be displayed within each user's list of sessions.",
+                                    },
+                                    {
+                                        name: "users",
+                                        type: "{message}",
+                                        optional: false,
+                                        description:
+                                            "The initial state of all users in the session.",
+                                    },
+                                    {
+                                        name: "steamid",
+                                        type: "uint64",
+                                        optional: false,
+                                        description:
+                                            "(Optional) steamid to make the request on behalf of -- if specified, the user must be in the session and all users being added to the session must be friends with the user.",
+                                    },
+                                ],
+                            },
+                        ],
+                    },
                 ],
             },
         };
